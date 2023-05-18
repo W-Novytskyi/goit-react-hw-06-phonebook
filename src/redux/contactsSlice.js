19683ad86b98ae47ae294078a5b5e29474ed3c23
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-05-19 01:22:36 +0300

    + неробочий Redux Persist

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -29,3 +29,46 @@ const contactsSlice = createSlice({
 
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
+
+// import { createSlice, nanoid } from '@reduxjs/toolkit';
+// import { persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// const contactsInitialState = [];
+
+// const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: contactsInitialState,
+//   reducers: {
+//     addContact: {
+//       reducer(state, action) {
+//         state.push(action.payload);
+//       },
+//       prepare(name, number) {
+//         return {
+//           payload: {
+//             id: nanoid(),
+//             name,
+//             number,
+//           },
+//         };
+//       },
+//     },
+//     deleteContact(state, action) {
+//       const index = state.findIndex(contact => contact.id === action.payload);
+//       state.splice(index, 1);
+//     },
+//   },
+// });
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
+// export const contactsReducer = persistReducer(
+//   persistConfig,
+//   contactsSlice.reducer
+// );
+
+// export const { addContact, deleteContact } = contactsSlice.actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,3 +8,32 @@ export const store = configureStore({
     filters: filtersReducer,
   },
 });
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsReducer } from './contactsSlice';
+// import {
+//   persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import { filtersReducer } from './filtersSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filters: filtersReducer,
+//   },
+//   middleware(getDefaultMiddleware) {
+//     return getDefaultMiddleware({
+//       serializabelCheck: {
+//         ignoredAction: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     });
+//   },
+// });
+
+// export const persistor = persistStore(store);
